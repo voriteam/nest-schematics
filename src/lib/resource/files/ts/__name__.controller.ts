a16a22c2130e802f776073e159c4fcd304289033
Clinton Blackburn <clintonb@vori.com>
2024-05-08 18:32:52 -0700

    fix(resource): Passing named parameters to service
    Controllers now pass named parameters to service methods. The controller test file is no longer generated since we prefer end-to-end tests.

--- src/lib/resource/files/ts/__name__.controller.ts
+++ src/lib/resource/files/ts/__name__.controller.ts
@@ -30,7 +30,7 @@ export class <%= classify(name) %>Controller {
     @Req() request: RequestWithBannerUser,
     @Body() create<%= singular(classify(name)) %>Dto: Create<%= singular(classify(name)) %>Dto
   ): Promise<<%= singular(classify(name)) %>Dto> {
-    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.create(request.user, create<%= singular(classify(name)) %>Dto);
+    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.create({bannerID: request.user.bannerID, dto: create<%= singular(classify(name)) %>Dto});
     return new <%= singular(classify(name)) %>Dto(<%= singular(camelize(name)) %>);
   }
 
@@ -38,7 +38,7 @@ export class <%= classify(name) %>Controller {
   @ApiOkResponse({ type: <%= singular(classify(name)) %>Dto, isArray: true })
   @Get()
   public async findAll(@Req() request: RequestWithBannerUser): Promise<<%= singular(classify(name)) %>Dto[]> {
-    const <%= camelize(name) %> = await this.<%= lowercased(name) %>Service.findAll(request.user);
+    const <%= camelize(name) %> = await this.<%= lowercased(name) %>Service.findAll({bannerID: request.user.bannerID});
     return <%= camelize(name) %>.map(<%= singular(camelize(name)) %> => new <%= singular(classify(name)) %>Dto(<%= singular(camelize(name)) %>));
   }
 
@@ -47,7 +47,7 @@ export class <%= classify(name) %>Controller {
   @ApiOkResponse({ type: <%= singular(classify(name)) %>Dto })
   @Get(':id')
   public async findOne(@Req() request: RequestWithBannerUser, @Param() params: FindOneParams) {
-    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.findOne(request.user, params.id);
+    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.findOne({bannerID: request.user.bannerID, id: params.id});
     return new <%= singular(classify(name)) %>Dto(<%= singular(camelize(name)) %>);
   }
 
@@ -57,7 +57,7 @@ export class <%= classify(name) %>Controller {
   @ApiOkResponse({ type: <%= singular(classify(name)) %>Dto })
   @Patch(':id')
   public async update(@Req() request: RequestWithBannerUser, @Param() params: FindOneParams, @Body() update<%= singular(classify(name)) %>Dto: Update<%= singular(classify(name)) %>Dto) {
-    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.update(request.user, params.id, update<%= singular(classify(name)) %>Dto);
+    const <%= singular(camelize(name)) %> = await this.<%= lowercased(name) %>Service.update({bannerID: request.user.bannerID, id: params.id, dto: update<%= singular(classify(name)) %>Dto});
     return new <%= singular(classify(name)) %>Dto(<%= singular(camelize(name)) %>);
   }
 
@@ -66,7 +66,7 @@ export class <%= classify(name) %>Controller {
   @ApiNoContentResponse()
   @Delete(':id')
   public async remove(@Req() request: RequestWithBannerUser, @Param() params: FindOneParams): Promise<void> {
-    await this.<%= lowercased(name) %>Service.remove(request.user, params.id);
+    await this.<%= lowercased(name) %>Service.remove({bannerID: request.user.bannerID, id: params.id});
   }<% } else if (type === 'microservice' && crud) { %>
 
   @MessagePattern('create<%= singular(classify(name)) %>')
